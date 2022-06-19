9c1a089b583da92f8ba2fcb6233480e43d092331
G. J. Yeomans <sirtuxalot@gmail.com>
2022-06-19 14:01:46 -0400

    Update working sketch and ReadMe for branch

--- BG_DIY_WxStn.ino
+++ BG_DIY_WxStn.ino
@@ -1,7 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 BG_DIY_WxStn.ino (InfluxDB)
 Author: sirtuxalot@gmail.com
-Last Updated: 07 Jun 2022
+Last Updated: 19 Jun 2022
 Notes: Adding code to utilize InfluxDb and WifiManager
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -17,31 +17,31 @@ Notes: Adding code to utilize InfluxDb and WifiManager
 
 // Libraries
 
-#include <Wire.h>                      // common library for all I2C stuf
-#include <DHT.h>                       // library for DHT11
-#include <SFE_BMP180.h>                // library for BMP180
-#include <BH1750.h>                    // library for BH1750 (AKA GY-30)
-#include <Adafruit_SI1145.h>           // library for SI1145
-#include <ESP8266WiFi.h>               // library for web access
-#include <InfluxDbClient.h>            // library for sending data to influxdb
-#include <WiFiManager.h>               // library for managing wifi connectivity
+#include <Wire.h>                                     // common library for all I2C stuf
+#include <DHT.h>                                      // library for DHT11
+#include <SFE_BMP180.h>                               // library for BMP180
+#include <BH1750.h>                                   // library for BH1750 (AKA GY-30)
+#include <Adafruit_SI1145.h>                          // library for SI1145
+#include <ESP8266WiFi.h>                              // library for web access
+#include <InfluxDbClient.h>                           // library for sending data to influxdb
+#include <WiFiManager.h>                              // library for managing wifi connectivity
 
 // Defines
 
-const int DHTPIN {14};                 // pin for DHT11
-const char DHTTYPE {DHT11};            // name for DHT11
-DHT dht(DHTPIN, DHTTYPE);              // initialize DHT11 sensor
-const char RainSensor {A0};            // pin for LM393 Rain Sensor
-const float ALTITUDE {171.0};          // Altitude of my location in meters
-SFE_BMP180 pressure;                   // name for BMP180 
-BH1750 lightMeter;                     // name for BH1750 (AKA GY-30)
-Adafruit_SI1145 uv;                    // name for SI1145
-WiFiClient client;                     // use as wifi client
-#define INFLUXDB_URL "influxdb"         // InfluxDB  server url. Don't use localhost, always server name or ip address.
-//#define INFLUXDB_TOKEN "toked-id"    // InfluxDB 2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
-//#define INFLUXDB_ORG "org"           // InfluxDB 2 organization id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
-//#define INFLUXDB_BUCKET "bucket"     // InfluxDB 2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
-#define INFLUXDB_DB_NAME "collectd"    // InfluxDB v1 database name
+const int DHTPIN {14};                                // pin for DHT11
+const char DHTTYPE {DHT11};                           // name for DHT11
+DHT dht(DHTPIN, DHTTYPE);                             // initialize DHT11 sensor
+const char RainSensor {A0};                           // pin for LM393 Rain Sensor
+const float ALTITUDE {171.0};                         // Altitude in meters
+SFE_BMP180 pressure;                                  // name for BMP180 
+BH1750 lightMeter;                                    // name for BH1750 (AKA GY-30)
+Adafruit_SI1145 uv;                                   // name for SI1145
+#define INFLUXDB_URL "http://192.168.254.203:8096"    // URL for server, and TCP port for influxdb application
+#define INFLUXDB_DB_NAME "weather"                    // InfluxDB v1 database name
+//#define INFLUXDB_TOKEN "toked-id"                   // InfluxDB 2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
+//#define INFLUXDB_ORG "org"                          // InfluxDB 2 organization id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
+//#define INFLUXDB_BUCKET "bucket"                    // InfluxDB 2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+#define TZ_INFO "EST5EDT"                             // Set Time Zone for InfluxDB
 
 // Variables
 
@@ -49,9 +49,15 @@ float temp = 0;
 float humid = 0;
 float pres = 0;
 int light = 0;
-unsigned long LastEntry;
 int RainSensorValue = 0;
 float unIndex = 0;
+
+// Setup connection to InfluxDB instance (v1)
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+// Setup connection to InfluxDB instance (v2)
+//InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+
+Point sensor("weather");
 
 void setup() {
 
@@ -68,12 +74,12 @@ void setup() {
   uv.begin();
 
   // connect to wireless network
-  Serial.begin(115200);                // Begin Serial Communication with 115200 Baud Rate
+  Serial.begin(115200);                               // Begin Serial Communication with 115200 Baud Rate
   Serial.println();
-  WiFi.mode(WIFI_STA);                 // Configure ESP8266 in STA Mode
-  WiFiManager wfm;                     // Initialize WiFiManager
-  //wfm.resetSettings();               // Uncomment to reset all settings when testing
-  wfm.setHostname("wx_station");       // Set hostname
+  WiFi.mode(WIFI_STA);                                // Configure ESP8266 in STA Mode
+  WiFiManager wfm;                                    // Initialize WiFiManager
+  //wfm.resetSettings();                              // Uncomment to reset all settings when testing
+  wfm.setHostname("wx_station");                      // Set hostname
   bool result;
   result = wfm.autoConnect("WxStnAP");
   if (!result) {
@@ -84,20 +90,37 @@ void setup() {
     //Serial.println(ssid);
   }
   Serial.println();
+  // Syncing progress and the time will be printed to Serial
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+  // Add constant tags - only once
+  sensor.addTag("device", "wx_station");
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
   WxStats();
-  ESP.deepSleep(300e6);
+  ESP.deepSleep(120e6);                               // Deep sleep for 2 minutes
 }
 
 void WxStats() {
+  // Store measured value into point
+  sensor.clearFields();
+
   // read DHT11 sensor
   debugln();
   float humid = dht.readHumidity();
+  sensor.addField("humidity",humid);
   debug("humidity: ");
   debug(humid);
   debugln();
 
   // Read temperature as Fahrenheit (isFahrenheit = true)
   float temp = dht.readTemperature(true);
+  sensor.addField("temperature",temp);
   // Check if any reads failed and exit early (to try again).
   if (isnan(humid) || isnan(temp)) {
 	  Serial.println(F("Failed to read from DHT sensor!"));
@@ -106,6 +129,7 @@ void WxStats() {
 
   // Compute heat index in Fahrenheit (the default)
   float hif = dht.computeHeatIndex(temp, humid);
+  sensor.addField("heatindex",hif);
   debug("heat index: ");
   debug(hif);
   debugln();
@@ -117,21 +141,14 @@ void WxStats() {
   // read LM393 Rain Sensor
   RainSensorValue = analogRead(RainSensor);
   debug("moisture: ");
-  if (RainSensorValue <= 100) {
-    debug("Heavy Rain - ");
-    if (RainSensorValue > 100 and RainSensorValue <= 400) {
-      debug("Raining - ");
-      if (RainSensorValue > 400 and RainSensorValue <= 800) {
-        debug("Light Rain - ");
-      }
-    }
-  }
+  sensor.addField("rain_intensity",RainSensorValue);
   debug(RainSensorValue);
   debugln();
 
   // read SI1145
   float uvIndex = uv.readUV();
   unIndex /= 100.0;
+  sensor.addField("uvindex",unIndex);
   debug("uv index: ");
   debug(uvIndex);
   debugln();
@@ -169,6 +186,7 @@ void WxStats() {
           // Print out the measurement:
           debug("absolute pressure: ");
           debug(P);
+          sensor.addField("abs_pressure",P);
           debug(" mb, ");
           debug(P*0.0295333727);
           debugln(" inHg");
@@ -176,6 +194,7 @@ void WxStats() {
           pres = pressure.sealevel(P,ALTITUDE);
           debug("relative (sea-level) pressure: ");
           debug(pres);
+          sensor.addField("sea_pressure",pres);
           debug(" mb, ");
           debug(pres*0.0295333727);
           debugln(" inHg");
@@ -199,7 +218,15 @@ void WxStats() {
   uint16_t light = lightMeter.readLightLevel();
   debug("light level: ");
   debug(light);
+  sensor.addField("light",light);
   debugln();
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 }
 
 void loop() {
--- ReadMe.md
+++ ReadMe.md
@@ -12,7 +12,13 @@ Common Downloaded Libraries
 - WiFi Manager - [https://github.com/tzapu/WiFiManager.git](https://github.com/tzapu/WiFiManager.git)
 
 Settings you will need to change for your station
-- const float ALTITUDE {171.0};          // Altitude of your location (in meters)
+- const float ALTITUDE {171.0};                       // Altitude of your location (in meters)
+- #define INFLUXDB_URL "http://192.168.254.203:8096"  // URL for server, and TCP port for influxdb application
+- #define INFLUXDB_DB_NAME "weather"                  // InfluxDB v1 database name
+- //#define INFLUXDB_TOKEN "toked-id"                 // InfluxDB 2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
+- //#define INFLUXDB_ORG "org"                        // InfluxDB 2 organization id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
+- //#define INFLUXDB_BUCKET "bucket"                  // InfluxDB 2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+- #define TZ_INFO "EST5EDT"                           // Correct timezone setting
 
 Revisions:
 - 11 July 2021: Code clean-up due to my OCD
@@ -22,4 +28,4 @@ Revisions:
 - 20 November 2021: Added rain sensor with assistance from [here](https://www.youtube.com/watch?v=2layMOhue7M&t)
 - 23 November 2021: Added deep sleep with assistance from [here](https://randomnerdtutorials.com/esp8266-deep-sleep-with-arduino-ide/). Replace Serial(ln) with Debug(ln) to allow enable/disable of debug content in serial monitor
 - 22 December 2021: Standardize format of constants with the sketch.  Added code for SI1145 UV sensor
-- 7 June 2022: Add libraries and code to send data to influxdb and wifimanager to simply network setup
+- 19 June 2022: Add libraries and code to send data to influxdb and wifimanager to simply network setup
